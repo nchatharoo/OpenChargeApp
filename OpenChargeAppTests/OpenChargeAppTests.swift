38443285d06df58298ca660c0909a2b96671f681
Nadheer Chatharoo <>
2021-09-02 11:23:04 +0200

    OpenChargeLoader does not request data on creation

--- OpenChargeAppTests/OpenChargeAppTests.swift
+++ OpenChargeAppTests/OpenChargeAppTests.swift
@@ -8,26 +8,24 @@
 import XCTest
 @testable import OpenChargeApp
 
+class HTTPClient {
+    var requestedURL: URL?
+
+}
+
+class OpenChargeLoader {
+    
+}
+
 class OpenChargeAppTests: XCTestCase {
+    
+    private let baseAPIURL = "https://api.openchargemap.io/v3/poi/"
+    private let apiKey = "6bdc7787-1e5b-4567-920a-9a77632ccb96"
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_doesNotRequestDataOnCreation() {
+        let client = HTTPClient()
+        let _ = OpenChargeLoader()
+        
+        XCTAssertNil(client.requestedURL)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
