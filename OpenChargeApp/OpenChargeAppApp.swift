775ed70c2082119cc27712b454dee7b5e92a5e2d
Nadheer Chatharoo <>
2021-09-02 11:15:13 +0200

    Initial commit

---
+++ OpenChargeApp/OpenChargeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenChargeAppApp.swift
+//  OpenChargeApp
+//
+//  Created by Nadheer on 02/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct OpenChargeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
