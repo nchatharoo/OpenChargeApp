213ca820207b66bca66c1ca1be6326d86415a4d7
Nadheer Chatharoo <>
2021-09-02 11:50:35 +0200

    Move test logic to a separate type HTTPClientSpy

--- OpenChargeAppTests/OpenChargeAppTests.swift
+++ OpenChargeAppTests/OpenChargeAppTests.swift
@@ -9,17 +9,23 @@ import XCTest
 @testable import OpenChargeApp
 
 class HTTPClient {
-    
-    var requestedURL: URL?
-    static let shared = HTTPClient()
-    
-    private init() {}
+    static var shared = HTTPClient()
+
+    func get(from url: URL) { }
 }
 
 class OpenChargeLoader {
     
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+    }
+}
+
+class HTTPClientSpy: HTTPClient {
+    var requestedURL: URL?
+
+    override func get(from url: URL) {
+        requestedURL = url
     }
 }
 
@@ -29,14 +35,16 @@ class OpenChargeAppTests: XCTestCase {
     private let apiKey = "6bdc7787-1e5b-4567-920a-9a77632ccb96"
 
     func test_doesNotRequestDataOnCreation() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let _ = OpenChargeLoader()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = OpenChargeLoader()
         
         sut.load()
