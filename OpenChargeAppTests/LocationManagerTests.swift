6eccf5466cc20af9b50c2fcc1f6cf205ed751666
Nadheer Chatharoo <>
2021-09-09 21:07:19 +0200

    request location with mock object

--- OpenChargeAppTests/LocationManagerTests.swift
+++ OpenChargeAppTests/LocationManagerTests.swift
@@ -6,27 +6,99 @@
 //
 
 import XCTest
+import CoreLocation
+
+protocol LocationManagerInterface {
+    var locationManagerDelegate: LocationManagerDelegate? { get set }
+    func requestWhenInUseAuthorization()
+}
+
+protocol LocationManagerDelegate: AnyObject {
+    func locationManager(_ manager: LocationManagerInterface, didUpdateLocations locations: [CLLocation])
+}
+
+extension CLLocationManager: LocationManagerInterface {
+    var locationManagerDelegate: LocationManagerDelegate? {
+        get { return delegate as! LocationManagerDelegate? }
+        set { delegate = newValue as! CLLocationManagerDelegate? }
+    }
+}
+
+class LocationManager: NSObject {
+
+    var locationManager: LocationManagerInterface
+    
+    private var currentLocationCallback: ((CLLocation) -> Void)?
+    
+    init(locationManager: LocationManagerInterface = CLLocationManager()) {
+        self.locationManager = locationManager
+        super.init()
+        self.locationManager.locationManagerDelegate = self
+    }
+
+    func requestLocationCompletion(completion: @escaping (CLLocation) -> Void) {
+        currentLocationCallback = {  (location) in
+            completion(location)
+        }
+        self.locationManager.requestWhenInUseAuthorization()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.locationManager(manager, didUpdateLocations: locations)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.locationManager(manager, didFailWithError: error)
+    }
+}
+
+extension LocationManager: LocationManagerDelegate {
+    func locationManager(_ manager: LocationManagerInterface, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        self.currentLocationCallback?(location)
+        self.currentLocationCallback = nil
+    }
+}
 
 class LocationManagerTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    struct LocationManagerMock: LocationManagerInterface {
+        
+        var locationManagerDelegate: LocationManagerDelegate?
+            
+        var locationToReturn: (()->CLLocation)?
+        
+        func requestWhenInUseAuthorization() {
+            guard let location = locationToReturn?() else { return }
+            locationManagerDelegate?.locationManager(self, didUpdateLocations: [location])
         }
     }
+        
+    func test_delegateIsNotNil() {
+        let sut = LocationManager()
+        
+        XCTAssertNotNil(sut.locationManager.locationManagerDelegate)
+    }
+    
+    func test_requestLocation() {
+        var mock = LocationManagerMock()
 
+        mock.locationToReturn = {
+            return CLLocation(latitude: 10.0, longitude: 10.0)
+        }
+        
+        let sut = LocationManager(locationManager: mock)
+
+        let expectedLocation = CLLocation(latitude: 10.0, longitude: 10.0)
+        let completionExpectation = expectation(description: "completion expectation")
+                
+        sut.requestLocationCompletion { (location) in
+            completionExpectation.fulfill()
+            XCTAssertEqual(location.coordinate.latitude,expectedLocation.coordinate.latitude)
+            XCTAssertEqual(location.coordinate.longitude,expectedLocation.coordinate.longitude)
+        }
+        wait(for: [completionExpectation], timeout: 1)
+    }
 }
