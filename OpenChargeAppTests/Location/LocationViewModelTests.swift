c61136db8429e9e355b39d4962c1faca7c058d21
Nadheer Chatharoo <>
2021-09-13 08:40:56 +0200

    Init does not get location

--- OpenChargeAppTests/Location/LocationViewModelTests.swift
+++ OpenChargeAppTests/Location/LocationViewModelTests.swift
@@ -6,7 +6,28 @@
 //
 
 import XCTest
+import OpenChargeApp
+import MapKit
+
+class LocationViewModel: NSObject {
+    let locationManager: LocationManager
+    var coordinateRegion: MKCoordinateRegion?
+
+    init(locationManager: LocationManager) {
+        self.locationManager = locationManager
+    }
 
 class LocationViewModelTests: XCTestCase {
+    
+    func test_initDoesNotGetLocation() {
+        let mock = LocationManagerMock()
+        mock.locationToReturn = {
+            return CLLocation(latitude: 10, longitude: 10)
+        }
+        let locationManager = LocationManager(locationManager: mock)
+        let sut = LocationViewModel(locationManager: locationManager)
+        
+        XCTAssertNil(sut.coordinateRegion)
+    }
 
 }
