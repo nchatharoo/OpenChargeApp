9c65bcd3beb73fe974bb9412b1072e6c7c51ec84
Nadheer Chatharoo <>
2021-09-13 21:06:47 +0200

    Add OpenChargeViewModel

---
+++ OpenChargeAppTests/API/OpenChargeViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  OpenChargeViewModel.swift
+//  OpenChargeAppTests
+//
+//  Created by Nadheer on 13/09/2021.
+//
+
+import XCTest
+import OpenChargeApp
+
+class OpenChargeViewModel: XCTestCase {
+
+    
+}
