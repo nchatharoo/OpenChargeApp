c38482dd2e657c31627610895f8fb6156dcccacb
Nadheer Chatharoo <nchatharoo@icloud.com>
2022-02-11 11:24:46 +0100

    Add extensions to their own file

--- OpenChargeApp/Extension/Shape.swift
+++ OpenChargeApp/Extension/Shape.swift
@@ -5,4 +5,15 @@
 //  Created by Nadheer on 11/02/2022.
 //
 
-import Foundation
+import SwiftUI
+
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
--- OpenChargeApp/Extension/View.swift
+++ OpenChargeApp/Extension/View.swift
@@ -5,4 +5,10 @@
 //  Created by Nadheer on 11/02/2022.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
