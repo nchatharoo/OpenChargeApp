ecb1cb0d3d670bce6076476b528aff6df008a032
Nadheer Chatharoo <nchatharoo@icloud.com>
2022-02-11 11:24:19 +0100

    Add extensions to their own file

---
+++ OpenChargeApp/Extension/Shape.swift
@@ -0,0 +1,8 @@
+//
+//  Shape.swift
+//  OpenChargeApp
+//
+//  Created by Nadheer on 11/02/2022.
+//
+
+import Foundation
---
+++ OpenChargeApp/Extension/View.swift
@@ -0,0 +1,8 @@
+//
+//  View.swift
+//  OpenChargeApp
+//
+//  Created by Nadheer on 11/02/2022.
+//
+
+import Foundation
