f0ba1bbc0c5c6aef50684986279090bb9f45099f
Nadheer Chatharoo <nchatharoo@icloud.com>
2022-02-16 10:24:07 +0100

    Display an Alert when network error

--- OpenChargeApp/ChargePointViewModel.swift
+++ OpenChargeApp/ChargePointViewModel.swift
@@ -10,6 +10,12 @@ import MapKit
 import Combine
 import SwiftUI
 
+struct NetworkError: Error, Identifiable {
+    let id = UUID()
+    let title = "Error"
+    var message = ""
+}
+
 final public class ChargePointViewModel: ObservableObject {
     let client: HTTPClient
     private let baseAPIURL = URL(string: "https://api.openchargemap.io/v3/poi/")!
@@ -17,6 +23,7 @@ final public class ChargePointViewModel: ObservableObject {
     @Published public var items = Charge()
     @Published var isProcessing = false
     @Published var query = ""
+    @Published var networkError: NetworkError?
     
     private lazy var dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
@@ -39,16 +46,16 @@ final public class ChargePointViewModel: ObservableObject {
         isProcessing = true
         
         cancellables = client.getPublisher(from: baseAPIURL, with: coordinate)
-            .map { (data: Data, response: HTTPURLResponse) in
+            .tryMap { (data: Data, response: HTTPURLResponse) in
                 data
             }
             .decode(type: Charge.self, decoder: jsonDecoder)
             .receive(on: RunLoop.main)
             .sink(receiveCompletion: { [weak self] completion in
                 guard let self = self else { return }
-                switch completion {
-                case .finished: break
-                case let .failure(error): print(error) }
+                if case .failure(let error) = completion {
+                    self.networkError = NetworkError(message: "Details: \(error.localizedDescription)")
+                }
                 self.isProcessing = false
             }, receiveValue: { [weak self] items in
                 guard let self = self else { return }
