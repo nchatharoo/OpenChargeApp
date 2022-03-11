6c063e166e98bea3b86a5fa4da5412ff2b0cbd3f
Nadheer Chatharoo <nchatharoo@icloud.com>
2022-03-11 09:45:30 +0100

    Add ChargerViewModelTests

---
+++ OpenChargeAppTests/ChargerViewModelTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChargerViewModelTests.swift
+//  OpenChargeAppTests
+//
+//  Created by Nadheer on 11/03/2022.
+//
+
+import XCTest
+
+class ChargerViewModelTests: XCTestCase {
+
+    func test() {
+        XCTFail()
+    }
+
+}
