441840c6a9bb42e92a965991e4e380f856a8ee6b
Nadheer Chatharoo <nchatharoo@icloud.com>
2022-03-14 10:57:51 +0100

    Inline variables

--- OpenChargeAppTests/ChargerViewModelTests.swift
+++ OpenChargeAppTests/ChargerViewModelTests.swift
@@ -270,6 +270,34 @@ class ChargerViewModelTests: XCTestCase {
     }
     
     private func uniqueCharger() -> Charger {
-        return Charger(dataProvider: nil, operatorInfo: OperatorInfo(websiteURL: "http://a-url.com", comments: "any comment", phonePrimaryContact: "0123456789", phoneSecondaryContact: "0123456789", isPrivateIndividual: nil, addressInfo: nil, bookingURL: nil, contactEmail: nil, faultReportEmail: nil, isRestrictedEdit: nil, id: nil, title: "any title"), usageType: UsageType(isPayAtLocation: true, isMembershipRequired: false, isAccessKeyRequired: true, id: nil, title: "a usage type title"), statusType: nil, submissionStatus: nil, userComments: nil, percentageSimilarity: nil, mediaItems: nil, isRecentlyVerified: nil, dateLastVerified: nil, id: nil, uuid: UUID().uuidString, parentChargePointID: nil, dataProviderID: nil, dataProvidersReference: nil, operatorID: nil, operatorsReference: nil, usageTypeID: nil, usageCost: "a usage cost", addressInfo: AddressInfo(id: nil, title: "any address title", addressLine1: "any address line", addressLine2: nil, town: "any town", stateOrProvince: "any state", postcode: "any postcode", countryID: nil, country: nil, latitude: 0.0, longitude: 0.0, contactTelephone1: "0123456789", contactTelephone2: nil, contactEmail: nil, accessComments: nil, relatedURL: nil, distance: 0.1, distanceUnit: nil), connections: [Connection(id: nil, connectionTypeID: nil, connectionType: ConnectionType(formalName: "a formalName", isDiscontinued: nil, isObsolete: nil, id: nil, title: "a connection title"), reference: nil, statusTypeID: nil, statusType: StatusType(isOperational: true, isUserSelectable: true, id: nil, title: "a status type"), levelID: nil, level: Level(comments: "a level comment", isFastChargeCapable: true, id: nil, title: "a level title"), amps: 10, voltage: 10, powerKW: 10.0, currentTypeID: nil, currentType: CurrentType(currentTypeDescription: "a current type description", id: nil, title: "a current type title"), quantity: 2, comments: "a current type comment")], numberOfPoints: nil, generalComments: nil, datePlanned: nil, dateLastConfirmed: nil, statusTypeID: nil, dateLastStatusUpdate: Date(), dataQualityLevel: nil, dateCreated: Date(), submissionStatusTypeID: nil)
+        return Charger(dataProvider: nil,
+                       operatorInfo: OperatorInfo(websiteURL: "http://a-url.com", comments: "any comment", phonePrimaryContact: "0123456789", phoneSecondaryContact: "0123456789", isPrivateIndividual: nil, addressInfo: nil, bookingURL: nil, contactEmail: nil, faultReportEmail: nil, isRestrictedEdit: nil, id: nil, title: "any title"), usageType: UsageType(isPayAtLocation: true, isMembershipRequired: false, isAccessKeyRequired: true, id: nil, title: "a usage type title"),
+                       statusType: nil,
+                       submissionStatus: nil,
+                       userComments: nil,
+                       percentageSimilarity: nil,
+                       mediaItems: nil,
+                       isRecentlyVerified: nil,
+                       dateLastVerified: nil,
+                       id: nil,
+                       uuid: UUID().uuidString,
+                       parentChargePointID: nil,
+                       dataProviderID: nil,
+                       dataProvidersReference: nil,
+                       operatorID: nil,
+                       operatorsReference: nil,
+                       usageTypeID: nil,
+                       usageCost: "a usage cost",
+                       addressInfo: AddressInfo(id: nil, title: "any address title", addressLine1: "any address line", addressLine2: nil, town: "any town", stateOrProvince: "any state", postcode: "any postcode", countryID: nil, country: nil, latitude: 0.0, longitude: 0.0, contactTelephone1: "0123456789", contactTelephone2: nil, contactEmail: nil, accessComments: nil, relatedURL: nil, distance: 0.1, distanceUnit: nil),
+                       connections: [Connection(id: nil, connectionTypeID: nil, connectionType: ConnectionType(formalName: "a formalName", isDiscontinued: nil, isObsolete: nil, id: nil, title: "a connection title"), reference: nil, statusTypeID: nil, statusType: StatusType(isOperational: true, isUserSelectable: true, id: nil, title: "a status type"), levelID: nil, level: Level(comments: "a level comment", isFastChargeCapable: true, id: nil, title: "a level title"), amps: 10, voltage: 10, powerKW: 10.0, currentTypeID: nil, currentType: CurrentType(currentTypeDescription: "a current type description", id: nil, title: "a current type title"), quantity: 2, comments: "a current type comment")],
+                       numberOfPoints: nil,
+                       generalComments: nil,
+                       datePlanned: nil,
+                       dateLastConfirmed: nil,
+                       statusTypeID: nil,
+                       dateLastStatusUpdate: Date(),
+                       dataQualityLevel: nil,
+                       dateCreated: Date(),
+                       submissionStatusTypeID: nil)
     }
 }
